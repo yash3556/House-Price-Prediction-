eb4b78d5ecb5bf7e2ead4a4c47347a269ca7f6ef
yash3556 <yashkush660@gmail.com>
2025-12-29 10:42:11 +0530

    Implement RandomForestRegressor for house price prediction
    Added RandomForestRegressor model for house price prediction and implemented slider for feature selection with min/max values.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from sklearn.ensemble import RandomForestRegressor
 import streamlit as st
 import pickle
 from sklearn.preprocessing import StandardScaler
@@ -20,10 +21,35 @@ st.sidebar.title('Select House Features: ')
 st.sidebar.image('https://cdn.dribbble.com/userupload/20000742/file/original-aaf23458355a156d0cf85b8217a5065a.gif')
 all_value = []
 for i in final_x:
-  result = st.sidebar.slider(f'Select {i} value')
+  min_value = final_x[i].min()
+  max_value = final_x[i].max()
+  
+  result = st.sidebar.slider(f'Select {i} value',min_value,max_value)
   all_value.append(result)
 
-st.write(all_value)
+user_x = scaler.transform([all_value])
+@st.cache_data
+def ml_model(x,y):
+  model = RandomForestRegressor()
+  model.fit(x,y)
+  return model
+
+model = ml_model(scaled_x,y)
+house_price = model.predict(user_x)[0]
+
+final_price = round(house_price * 100000,2)
+with st.spinner('Predicting House Price'):
+  import time
+  time.sleep(2)
+
+st.success(f'Estimated House Price is : $ {final_price}')
+st.markdown('''**Design and Developed by : Yash Kushwaha**''')
+
+
+
+
+
+
 
 
 
